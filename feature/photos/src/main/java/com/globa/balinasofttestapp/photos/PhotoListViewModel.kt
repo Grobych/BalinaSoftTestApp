ce948adac2bd4a8d109002d3e77984065e3fb478
Alexander Globa <grobych@gmail.com>
2023-10-03 21:07:22 +0300

    code clean up

--- feature/photos/src/main/java/com/globa/balinasofttestapp/photos/PhotoListViewModel.kt
+++ feature/photos/src/main/java/com/globa/balinasofttestapp/photos/PhotoListViewModel.kt
@@ -2,17 +2,14 @@ package com.globa.balinasofttestapp.photos
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.globa.balinasofttestapp.camera.BitmapFormatter
 import com.globa.balinasofttestapp.login.api.AuthData
 import com.globa.balinasofttestapp.login.api.LoginRepository
 import com.globa.balinasofttestapp.photos.api.PhotosRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
@@ -20,16 +17,6 @@ class PhotoListViewModel @Inject constructor(
     private val photosRepository: PhotosRepository,
     private val loginRepository: LoginRepository
 ): ViewModel() {
-    fun uploadPhoto(bytes: ByteArray) {
-        viewModelScope.launch {
-            withContext(Dispatchers.Default) {
-                val base64 = BitmapFormatter.get64Image(bytes)
-
-                println(base64.length)
-            }
-        }
-    }
-
     private val _photosUiState = MutableStateFlow<PhotosUiState>(PhotosUiState.Loading)
     val photosUiState = _photosUiState.asStateFlow()
 
