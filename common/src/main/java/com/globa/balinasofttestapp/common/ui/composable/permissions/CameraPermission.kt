ace048f57e6102ebb23961e86104ff39025e9313
Alexander Globa <grobych@gmail.com>
2023-10-07 13:51:36 +0300

    update CameraPermission

--- common/src/main/java/com/globa/balinasofttestapp/common/ui/composable/permissions/CameraPermission.kt
+++ common/src/main/java/com/globa/balinasofttestapp/common/ui/composable/permissions/CameraPermission.kt
@@ -1,11 +1,20 @@
 package com.globa.balinasofttestapp.common.ui.composable.permissions
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.globa.balinasofttestapp.common.ui.theme.Paddings
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.isGranted
 import com.google.accompanist.permissions.rememberPermissionState
+import com.google.accompanist.permissions.shouldShowRationale
 
 @SuppressLint("PermissionLaunchedDuringComposition")
 @OptIn(ExperimentalPermissionsApi::class)
@@ -21,8 +30,24 @@ fun CameraPermission(
     if (cameraPermissionState.status.isGranted) {
         content()
     } else {
-        SideEffect {
-            cameraPermissionState.launchPermissionRequest()
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            val textToShow = if (cameraPermissionState.status.shouldShowRationale) {
+                "The camera is important for this app. Please grant the permission."
+            } else {
+                "Camera permission required for this feature to be available. " +
+                        "Please grant the permission"
+            }
+            Text(
+                text = textToShow,
+                modifier = Modifier.padding(Paddings.large)
+            )
+            Button(onClick = { cameraPermissionState.launchPermissionRequest() }) {
+                Text("Request permission")
+            }
         }
     }
 }
