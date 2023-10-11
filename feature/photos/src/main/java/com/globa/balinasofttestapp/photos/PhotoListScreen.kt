98a63a3a02b5340488b80a96dbe90054754cf265
Alexander Globa <grobych@gmail.com>
2023-10-11 22:51:28 +0300

    add placeholder for broken images

--- feature/photos/src/main/java/com/globa/balinasofttestapp/photos/PhotoListScreen.kt
+++ feature/photos/src/main/java/com/globa/balinasofttestapp/photos/PhotoListScreen.kt
@@ -1,6 +1,7 @@
 package com.globa.balinasofttestapp.photos
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -8,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.AlertDialog
@@ -22,7 +24,9 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.paging.LoadState
 import androidx.paging.PagingData
@@ -142,16 +146,21 @@ private fun DonePhotoListScreen(
                 ){
                     SubcomposeAsyncImage(
                         modifier = Modifier
-                            .fillMaxSize()
-                            .padding(start = Paddings.medium, end = Paddings.medium)
+                            .size(100.dp)
+                            .padding(start = Paddings.medium, end = Paddings.medium, top = Paddings.medium)
                             .combinedClickable(
                                 onClick = { onPhotoClick(it.id) },
                                 onLongClick = { onPhotoLongClick(it.id) }
                             )
-                            .clip(MaterialTheme.shapes.medium)
-                        ,
+                            .clip(MaterialTheme.shapes.medium),
                         model = it.url,
-                        loading = { LoadingAnimation(modifier = Modifier.fillMaxSize()) },
+                        loading = { LoadingAnimation() },
+                        error = {
+                            Image(
+                                painter = painterResource(id = com.globa.balinasofttestapp.common.R.drawable.ic_broken),
+                                contentDescription = "Image broken or not found"
+                            )
+                        },
                         contentDescription = "Photo ${it.id}"
                     )
                     Text(text = DateFormatter.getSimpleDate(it.date))
